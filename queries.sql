33b86ea265cd4be6bda96cc421da87c8e353ddd0
Sheikh <kenneth.sheikh@hotmail.com>
2025-11-05 22:30:29 +0100

    added queries

---
+++ queries.sql
@@ -0,0 +1,15 @@
+SELECT ISBN, Tittel, Forfatter
+FROM bok
+ORDER BY Tittel;
+
+SELECT u.UtlånsNr, u.ISBN, u.EksNr, u.Utlånsdato, l.Fornavn, l.Etternavn
+FROM utlån u
+JOIN låner l ON l.LNr = u.LNr
+WHERE u.Levert = 0
+ORDER BY u.Utlånsdato;
+
+SELECT b.Tittel, COUNT(e.EksNr) AS AntallEksemplarer
+FROM bok b
+LEFT JOIN eksemplar e ON e.ISBN = b.ISBN
+GROUP BY b.ISBN
+ORDER BY b.Tittel;
