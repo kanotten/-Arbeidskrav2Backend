458878c9b3f9abcba68a0947f2a7bd410bb4a1ce
Sheikh <kenneth.sheikh@hotmail.com>
2025-11-07 16:48:26 +0100

    task 4- 4D

--- main.py
+++ main.py
@@ -32,6 +32,38 @@ def sok_bok(conn, sokeord):
         print(row)
 
 
+def register_utlan(conn, lnr, isbn, eksnr, utlansdato):
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT 1 FROM eksemplar WHERE ISBN = ? AND EksNr = ?",
+        (isbn, eksnr),
+    )
+
+    if cursor.fetchone() is None:
+        print("kan ikke registrer utlån, ugyldig eksemplar")
+        return
+
+    cursor.execute('SELECT 1 FROM "låner" WHERE "LNr" = ?', (lnr,))
+    if cursor.fetchone() is None:
+        print("Kan ikke registrere utlån: låner finnes ikke.")
+        return
+
+    cursor.execute(
+        'SELECT 1 FROM "utlån" WHERE ISBN = ? AND EksNr = ? AND Levert = 0',
+        (isbn, eksnr),
+    )
+    if cursor.fetchnone() is None:
+        print("Eksemplar lånt ut")
+        return
+
+    cursor.execute(
+        'INSERT INTO "utlån" ("LNr","ISBN","EKsNr","Utlånsdato","Levert",) VALUES (?,?,?,?,0)',
+        (lnr, isbn, eksnr, utlansdato),
+    )
+    conn.commit()
+    print(f"Utlån registrert. UtlånsNr={cursor.lastrowid}")
+
+
 if __name__ == "__main__":
     conn = connect_to_database()
     print("Database connection established.\n")
