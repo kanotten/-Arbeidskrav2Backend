f1693cd55c3c5080ba71950fb1e321f78f6df1ad
Sheikh <kenneth.sheikh@hotmail.com>
2025-11-07 16:19:42 +0100

    task 4A-4C

--- main.py
+++ main.py
@@ -15,6 +15,23 @@ def vis_alle_bøker(conn):
         print(row)
 
 
+def sok_bok(conn, sokeord):
+    cursor = conn.cursor()
+    pattern = f"%{sokeord}%"
+    cursor.execute(
+        """
+           SELECT ISBN, Tittel, Forfatter
+           FROM bok
+           WHERE lower(Tittel) LIKE lower(?)
+              OR lower(Forfatter) LIKE lower(?)
+           """,
+        (pattern, pattern),
+    )
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
 if __name__ == "__main__":
     conn = connect_to_database()
     print("Database connection established.\n")
